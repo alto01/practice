5b0d3543b52528bc6c7cf606a9cfd50b2791fe23
alto01 <haratakumin@gmail.com>
2022-07-17 18:40:41 +0900

    add delete

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,10 @@
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
 Route::get('/posts/{post}','PostController@show');
+Route::get('/posts/{post}/edit','PostController@edit');
+
+Route::put('/posts/{post}', 'PostController@update');
 
 Route::post('/posts', 'PostController@store');
+
+Route::delete('/posts/{post}','PostController@delete');
